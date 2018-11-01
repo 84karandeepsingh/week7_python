fb66774609b82b1c86b70164c5ccf1d48df7299c
Thind <k_thind92494@tss-hr420-dm04.fc.ca>
2018-11-01 13:42:25 -0400

    adding functions

---
+++ functions.py
@@ -0,0 +1,20 @@
+# let's start and do some with functions
+# and also what they do
+
+
+def greeting():
+	# say hello
+	print("hello from your first function!")
+
+
+# this is how you call or invoke a function
+greeting()
+
+
+def greetings(msg="hello there!");
+    print("Our functions says",msg)
+
+
+greetings()
+greetings("This is an argument")
+greetings("why we are arguing?")
